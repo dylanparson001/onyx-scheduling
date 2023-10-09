f62a9f202dda430d7badee9062eb4c13197400d8
Dylan Parson <dpars74@wgu.edu>
2023-10-09 18:55:45 -0400

    API JWT generation and identity server

---
+++ api/OnyxScheduling/Auth/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnyxScheduling.Auth
+{
+    public class DataContext : IdentityDbContext<IdentityUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ api/OnyxScheduling/Auth/UserRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace OnyxScheduling.Auth
+{
+    public class UserRoles
+    {
+
+        public const string Admin = "Admin";
+        public const string Office = "Office";
+        public const string Field = "Field";
+    }
+}
